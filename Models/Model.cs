23e0a106d2daf582c8eeac2b6d154dc8031713a6
Brahim Bahaida <bbahieda@gmail.com>
2018-08-13 11:41:44 +0000

    initial commit and add models

---
+++ Models/Model.cs
@@ -0,0 +1,10 @@
+namespace vega_mosh_asp_angular.Models
+{
+    public class Model{
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
