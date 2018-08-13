2ede22db4296b02117edd7305a9f06c7a1f49491
Brahim Bahaida <bbahieda@gmail.com>
2018-08-13 13:46:03 +0000

    add persistence layer and the ef migrations

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega_mosh_asp_angular.Models
 {
     public class Make{
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public ICollection<Model> Models { get; set; }
---
+++ Persistence/VegaDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using vega_mosh_asp_angular.Models;
+
+namespace vega_mosh_asp_angular.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Make> Makes { get; set; }
+        
+    }
+}
